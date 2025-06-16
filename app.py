09d66771fd34d5e7c246151b610c6fa0705e0000
zack <nyagwayaezekiah@gmail.com>
2025-06-16 08:14:05 +0300

    stories page added

--- app.py
+++ app.py
@@ -3,7 +3,18 @@ from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
+    """render the home page"""
     return render_template('home.html')
+
+@app.route('/api/learning-tools', methods=['GET'])
+def learning_tools():
+    """render the learning tools page"""
+    return render_template('learning_tools.html')
+
+@app.route('/api/read-stories', methods=['GET'])
+def read_stories():
+    """render the read stories page"""
+    return render_template('read_stories.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
